c946e9c7f992bee3acd99948e3baa41b3867d544
Aaditya9646 <aadityanimbalkar9646@gmail.com>
2026-08-05 13:27:01 +0530

    Time: 0 ms (100%), Space: 15 MB (50.73%) - LeetHub

---
+++ 0330-patching-array/0330-patching-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minPatches(vector<int>& nums, int n) {
+        long long miss = 1;
+        int ans = 0, i = 0;
+
+        while(miss <= n){
+            if(i < nums.size() && nums[i] <= miss){
+                miss += nums[i++];
+            }else{
+                miss *= 2;
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+};
